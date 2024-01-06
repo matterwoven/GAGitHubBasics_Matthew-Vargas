be20d95010a9b22b9b2685aabd31f5feefce521c
matterwoven <145516753+matterwoven@users.noreply.github.com>
2024-01-05 17:54:00 -0800

    Add project files.

---
+++ GAGitHubBasics_Matthew Vargas/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GAGitHubBasics_Matthew_Vargas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Professor!\n----------------");
+            Console.WriteLine("What is your name? : '______' ");
+
+            string teacherName = Console.ReadLine();
+
+            Console.WriteLine("Glad to be in your class Professor " + teacherName + ",\n \nMy name is Matthew Vargas and my goals are to obtain skills to use for the building of tech applications, \nMy main interests lie in vr programs and how they are made\nI want to learn how to build basic applications so I can bridge out to more complicated concepts");
+        }
+        
+    }
+}
